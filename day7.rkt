8b68f1185a67d20b3ba6dbf6dbd84645c47dba46
gamburgm <mitch.gamburg@gmail.com>
2021-12-07 13:39:30 -0500

    ./finish

---
+++ day7.rkt
@@ -0,0 +1,56 @@
+#lang racket
+
+(require (for-syntax racket/base syntax/parse))
+
+;; Linear solution for part 1:
+;; 1. count the number of crabs to the left and right at each index
+;; 2. get fuel count at min idx
+;; 3. move right, adjusting the fuel count at each step based on # of crabs at left and right to calculate new fuel usage
+;; 4. return the minimum result
+
+(define-syntax (for/min stx)
+  (syntax-parse stx
+    [(_ for-clauses body ...+)
+     #`(begin
+         (define res
+           (for/fold ([n 'no-min])
+                     for-clauses
+             (let ([val (begin body ...)])
+               (if (eq? n 'no-min)
+                 val
+                 (min val n)))))
+         (if (eq? res 'no-min)
+           #f
+           res))]))
+
+(define (get-input port)
+  (map string->number (string-split (read-line port) ",")))
+
+(define (calculate-dist crabs pos travel-cost)
+  (define (cost-func n) (travel-cost n pos))
+  (apply + (map cost-func crabs)))
+
+(define (travel-cost1 n pos)
+  (abs (- n pos)))
+
+(define (travel-cost2 n pos)
+  (let ([res (abs (- n pos))])
+    (apply + (range 0 (add1 res)))))
+
+(define (calc1 crabs pos)
+  (calculate-dist crabs pos travel-cost1))
+
+(define (calc2 crabs pos)
+  (calculate-dist crabs pos travel-cost2))
+
+(define (leftmost-crab crabs)
+  (apply min crabs))
+
+(define (rightmost-crab crabs)
+  (apply max crabs))
+
+(define (get-best-pos crabs)
+  (for/min ([c (in-range (leftmost-crab crabs) (add1 (rightmost-crab crabs)))])
+    (calc2 crabs c)))
+
+(get-best-pos (get-input (current-input-port)))
